61c8c18084d594bed126aa3de8274348f2252a18
asmita2023 <146002469+asmita2023@users.noreply.github.com>
2025-06-17 11:53:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function clickme(){
+  document.getElementById("show").style.display="block"
+  document.getElementById("navbar").style.display="none"
+  document.getElementById("boby").opacit="0.6"
+}
+
+function cancel(){
+  document.getElementById("show").style.display="none"
+}
